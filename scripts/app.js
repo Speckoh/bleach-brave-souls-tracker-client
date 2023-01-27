2e359df4772699ce8c8c5fc53ad91b3058643252
speckoh <speckohart@gmail.com>
2023-01-27 17:02:43 -0500

    update an entity fix

--- scripts/app.js
+++ scripts/app.js
@@ -22,6 +22,9 @@ const indexCharacterContainer = document.querySelector('#value-container')
 const showCharacterContainer = document.querySelector('#show-character-container')
 // const deleteButton = document.querySelector('#delete-entry')
 
+let addingEntry = false;
+let entryToUpdate;
+
 indexCharacter()
 .then(res => res.json())
 .then(res => {
@@ -33,14 +36,53 @@ indexCharacter()
 //CREATE
 document.addEventListener('click', (event) => {
     event.preventDefault()
-    //Go to CREATE/UPDATE PAGE
+    //GO TO CREATE/UPDATE PAGE TO ADD
     if(event.target.matches('#add-new-entry-button')){
         console.log('bring up add page')
+        addingEntry = true;
         createUpdatePage.style.display = 'block'
         mainPage.style.display = 'none'
         document.querySelectorAll('.input-field').forEach( (event) => {
             event.firstChild.value = '';
         })
+    }
+    //GO TO CREATE/UPDATE PAGE TO EDIT
+    if(event.target.matches('#update-entry')){
+        console.log('bring up edit page')
+        addingEntry = false;
+        const id = event.target.getAttribute('data-id')
+        entryToUpdate = id;
+        createUpdatePage.style.display = 'block'
+        mainPage.style.display = 'none'
+        showCharacter(id)
+        .then((res) => res.json())
+        .then((res) => {
+            // console.log(res.character)
+            document.querySelector('#input-name').value = res.character.name
+            document.querySelector('#input-attribute').value = res.character.attribute
+            document.querySelector('#input-killer').value = res.character.killer
+            document.querySelector('#input-soulTrait').value = res.character.soulTrait
+        })
+		.catch(onFailure)
+        // //UPDATE
+// showCharacterContainer.addEventListener('submit', (event) => {
+// 	event.preventDefault()
+// 	const id = event.target.getAttribute('data-id')
+// 	const characterData = {
+// 		character: {
+// 			name: event.target['name'].value,
+//             attribute: event.target['attribute'].value,
+//             killer: event.target['killer'].value,
+//             soulTrait: event.target['soulTrait'].value,
+//             characterLinks: event.target['characterLinks'].value,
+//             slotLvls: event.target['slotLvls'].value
+// 		},
+// 	}
+//     if (!id){return}
+// 	updateCharacter(characterData, id)
+// 		.then(onUpdateCharacterSuccess)
+// 		.catch(onFailure)
+// })
     }
     //Return to MAIN PAGE
     if(event.target.matches('#return-button')){
@@ -48,25 +90,51 @@ document.addEventListener('click', (event) => {
         mainPage.style.display = 'block'
         createUpdatePage.style.display = 'none'
     }
-    //ADD/UPDATE Button
+    //ADD BUTTON to ADD THE ENTRY
     if(event.target.matches('#add-update-button')){
-        console.log('add/create button pressed')
-        const characterData = {
-            character:{
-                name: document.querySelector('#input-name').value,
-                attribute: document.querySelector('#input-attribute').value,
-                killer: document.querySelector('#input-killer').value,
-                soulTrait: document.querySelector('#input-soulTrait').value,
-                // accessories: event.target['ability'].value
-                characterLinks: document.querySelector('#input-link1').value,
-                slotLvls: document.querySelector('#input-slot1').value
+        if(addingEntry){
+            console.log('add/create button pressed')
+            const characterData = {
+                character:{
+                    name: document.querySelector('#input-name').value,
+                    attribute: document.querySelector('#input-attribute').value,
+                    killer: document.querySelector('#input-killer').value,
+                    soulTrait: document.querySelector('#input-soulTrait').value,
+                    // accessories: event.target['ability'].value
+                    characterLinks: document.querySelector('#input-link1').value,
+                    slotLvls: document.querySelector('#input-slot1').value
+                }
             }
+            createCharacter(characterData)
+            .then(onCreateCharacterSuccess)
+            .catch(onFailure)
         }
-        createCharacter(characterData)
-        .then(onCreateCharacterSuccess)
-        .catch(onFailure)
+        else if(!addingEntry){
+            console.log(entryToUpdate)
+            const characterData = {
+                character: {
+                    name: document.querySelector('#input-name').value,
+                    attribute: document.querySelector('#input-attribute').value,
+                    killer: document.querySelector('#input-killer').value,
+                    soulTrait: document.querySelector('#input-soulTrait').value
+                }
+            }
+            updateCharacter(characterData, entryToUpdate)
+            .then(onUpdateCharacterSuccess)
+            .then(function(){
+                refreshEntries(document.querySelector('#value-container'))
+                indexCharacter()
+                .then(res => res.json())
+                .then(res => {
+                onIndexCharacterSuccess(res.characters)
+                })
+            })
+            .catch(onFailure)
+            mainPage.style.display = 'block'
+            createUpdatePage.style.display = 'none'
+        }
     }
-    //DELETE Button
+    //DELETE ENTRY
     if(event.target.matches('#delete-entry')){
         const id = event.target.getAttribute('data-id')
         console.log(id)
--- scripts/ui.js
+++ scripts/ui.js
@@ -37,9 +37,11 @@ export const onIndexCharacterSuccess = (characters) => {
             <div>${character.characterLinks}</div>
             <div>${character.slotLvls}</div>
             <div id="action-container">
-                <button>U</button>
+                <button data-id="${character._id}" id="update-entry">
+                <img class="edit-icon" src="./assets/edit.png">
+                </button>
                 <button data-id="${character._id}" id="delete-entry">
-                <img class="trash-icon" src="./assets/trash.png" alt="trashcan">
+                <img class="trash-icon" src="./assets/trash.png">
                 </button>
             </div>
         </div>
@@ -51,25 +53,32 @@ export const onIndexCharacterSuccess = (characters) => {
 //SHOW - UPDATE - DELETE
 export const onShowCharacterSuccess = (character) => {
     const div = document.createElement('div')
-    div.innerHTML = `<h3>${character.name}</h3>
-    <p>${character.attribute}</p>
-    <p>${character._id}</p>
-
-    <form data-id="${character._id}">
-        <input type="text" name="name" value="${character.name}" />
-        <input type="text" name="attribute" value="${character.attribute}" />
-        <input type="text" name="killer" value="${character.killer}" />
-        <input type="text" name="soulTrait" value="${character.soulTrait}" />
-        <input type="text" name="characterLinks" value="${character.characterLinks}" />
-        <input type="text" name="slotLvls" value="${character.slotLvls}" />
-        <input type="submit" value="Update Character" />
-    </form>
-
-    <button data-id="${character._id}">Delete Character</button>
-    `
+    
     
     showCharacterContainer.appendChild(div)
 }
+
+// export const onShowCharacterSuccess = (character) => {
+//     const div = document.createElement('div')
+//     div.innerHTML = `<h3>${character.name}</h3>
+//     <p>${character.attribute}</p>
+//     <p>${character._id}</p>
+
+//     <form data-id="${character._id}">
+//         <input type="text" name="name" value="${character.name}" />
+//         <input type="text" name="attribute" value="${character.attribute}" />
+//         <input type="text" name="killer" value="${character.killer}" />
+//         <input type="text" name="soulTrait" value="${character.soulTrait}" />
+//         <input type="text" name="characterLinks" value="${character.characterLinks}" />
+//         <input type="text" name="slotLvls" value="${character.slotLvls}" />
+//         <input type="submit" value="Update Character" />
+//     </form>
+
+//     <button data-id="${character._id}">Delete Character</button>
+//     `
+    
+//     showCharacterContainer.appendChild(div)
+// }
 
 //UPDATE
 export const onUpdateCharacterSuccess = () => {
